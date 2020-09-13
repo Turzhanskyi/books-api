00cfdff3cad384cf6ede61a8d07c21280b48d391
Vitalii <turzhansky81@gmail.com>
2020-09-13 11:44:59 +0300

    Add Exception handling in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+
+  def not_destroyed(error)
+    render json: { errors: error.record.errors }, status: :unprocessable_entity
+  end
 end
