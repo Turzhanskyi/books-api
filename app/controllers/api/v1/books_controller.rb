34aad5694a8e6f2640b06573d81868b5a09b1642
Vitalii <turzhansky81@gmail.com>
2020-09-19 00:12:59 +0300

    Add BookRepresenter

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,7 +4,9 @@ module Api
   module V1
     class BooksController < ApplicationController
       def index
-        render json: Book.all
+        books = Book.all
+
+        render json: BooksRepresenter.new(books).as__json
       end
 
       def create
