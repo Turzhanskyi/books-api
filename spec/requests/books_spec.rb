398392956efba57e9bb318824c3de58cd4c6fe06
Vitalii <turzhansky81@gmail.com>
2020-10-02 02:00:49 +0300

    Add Response Body Specs & Use SpecHelper

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,16 +3,36 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
+  let(:first_author) { FactoryBot.create(:author, first_name: 'George', last_name: 'Orwell', age: 46) }
+  let(:second_author) { FactoryBot.create(:author, first_name: 'H.G', last_name: 'Wels', age: 78) }
+
   describe 'GET /books' do
     before do
-      create_list(:book, 2)
+      FactoryBot.create(:book, title: '1984', author: first_author)
+      FactoryBot.create(:book, title: 'The Time Machine', author: second_author)
     end
 
     it 'return all books' do
       get '/api/v1/books'
 
       expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body).size).to eq(2)
+      expect(response_body.size).to eq(2)
+      expect(response_body).to eq(
+        [
+          {
+            'id'          => 1,
+            'title'       => '1984',
+            'author_name' => 'George Orwell',
+            'author_age'  => 46
+          },
+          {
+            'id'          => 2,
+            'title'       => 'The Time Machine',
+            'author_name' => 'H.G Wels',
+            'author_age'  => 78
+          }
+        ]
+      )
     end
   end
 
@@ -26,11 +46,20 @@ describe 'Books API', type: :request do
       end.to change { Book.count }.from(0).to(1)
 
       expect(response).to have_http_status(:created)
+      expect(Author.count).to eq(1)
+      expect(response_body).to eq(
+        {
+          'id'          => 1,
+          'title'       => 'The Martian',
+          'author_name' => 'Andy Weir',
+          'author_age'  => 48
+        }
+      )
     end
   end
 
   describe 'DELETE /books/:id' do
-    let!(:book) { create(:book) }
+    let!(:book) { FactoryBot.create(:book, title: 1984, author: first_author) }
 
     it 'deletes a book' do
       expect do
